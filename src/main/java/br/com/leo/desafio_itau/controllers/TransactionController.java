e46c63bf97ec9e3a78e1770e9a1de5787cb941e5
leonardomuniz <leocontato@outlook.com.br>
2026-05-24 12:30:07 -0300

    feat: adding list endpoint

--- src/main/java/br/com/leo/desafio_itau/controllers/TransactionController.java
+++ src/main/java/br/com/leo/desafio_itau/controllers/TransactionController.java
@@ -1,33 +1,38 @@
 package br.com.leo.desafio_itau.controllers;
 
 import br.com.leo.desafio_itau.DTOs.InputTransactionDto;
+import br.com.leo.desafio_itau.entities.Transaction;
 import br.com.leo.desafio_itau.exceptions.BusinessRuleException;
 import br.com.leo.desafio_itau.services.transactions.TransactionInsertService;
+import br.com.leo.desafio_itau.services.transactions.TransactionListService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/transacao")
 public class TransactionController {
     private final TransactionInsertService transactionInsertService;
+    private final TransactionListService transactionListService;
 
     @Autowired
-    public TransactionController(TransactionInsertService transactionInsertService) {
+    public TransactionController(TransactionInsertService transactionInsertService, TransactionListService transactionListService) {
         this.transactionInsertService = transactionInsertService;
+        this.transactionListService = transactionListService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Transaction>> list() {
+        List<Transaction> response = transactionListService.run();
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping
     public ResponseEntity<Void> insert(@Valid @RequestBody InputTransactionDto input) {
-        boolean simular = true;
-        if (simular) {
-            throw new BusinessRuleException("Bora ver o que sai daqui");
-        }
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
